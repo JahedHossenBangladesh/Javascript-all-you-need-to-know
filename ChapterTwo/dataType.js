9b5e93156ea0a59f53b9df607aa7fddba3227334
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-06-22 13:52:22 +0600

    This is data type Number

---
+++ ChapterTwo/dataType.js
@@ -0,0 +1,18 @@
+// primitive data type 
+// 1.Number,
+// 2.String,
+// 3.backtick
+// 4.boolean 
+// 5.null
+// 6.undefined
+
+//All about Number
+
+var n = 23;
+var f = 33.33;
+var o = Number('45.34')
+
+console.log(Number.parseInt(o));// 45
+console.log(Number.parseFloat(o)); //45.34
+console.log(1/0);//infinity
+console.log('abec' *3) // NaN
