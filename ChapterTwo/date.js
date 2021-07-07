a1063b9f4eeb0b8bdf56842f718d44e8c7342830
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-07-07 11:46:46 +0600

    Math chapter 2 is finished

---
+++ ChapterTwo/date.js
@@ -0,0 +1,12 @@
+const date = new Date();
+
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleString());
+
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getMinutes());
+console.log(date.getSeconds())
+
--- ChapterTwo/math.js
+++ ChapterTwo/math.js
@@ -6,3 +6,9 @@ console.log(Math.abs(a)); // 3.4433
 console.log(Math.floor(a));// 3
 console.log(Math.ceil(a));//4  because of 3.44
 console.log(Math.round(a));// 3 if 3.5 then that will be 4 
+console.log(Math.max(333,55,666,232,66));
+console.log(Math.min(333,222,55,666,222));
+console.log(Math.pow(3,5));
+console.log(Math.sqrt(49));
+console.log(Math.round(Math.random() * (100-50) +1));
+
